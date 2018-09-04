3dd293ee8562b3b1ce2e4e97fe6182f428af71ee
unknown <bormanrenovation6191@gmail.com>
2018-09-05 02:17:46 +0300

    Was refctored route /v2/getallplaces

--- app/Http/Controllers/v2MainPageController.php
+++ app/Http/Controllers/v2MainPageController.php
@@ -24,24 +24,16 @@ class v2MainPageController extends Controller
     }
 
     public function getAllPlaces(){
-        return response()->json(  City::all()->map(function($city,$key){
-            return [
-                'city_name' => $city['name'],
-                'id' => $city['id'],
-                'places' => ($city['places']->map(function ($place,$key){
-                    return [
-                        'address' => $place['address'],
-                        'latitude' => $place['latitude'],
-                        'longitude' => $place['longitude'],
-                        'id' => $place['id'],
-                        'spaces' => $place['spaces']->map(function ($space,$key){
-                            return [ 'name' => $space['name'],
-                                'id' => $space['id']
-                            ];
-                        })->all()
-                    ];
-                }))->all()
+        $allPlaces = Place::all();
+        $arrPlaces = array();
+        foreach ($allPlaces as  $place ){
+            $arrPlaces[]= [
+                'name'=>$place->name,
+                'address'=>$place->address,
+                'longitude'=>$place->longitude,
+                'latitude'=>$place->latitude
             ];
-        })->all() );
+        }
+        return response()->json($arrPlaces,200);
     }
 }
