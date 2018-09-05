d826d09aa639d46c000baa22141bf51257006c15
Che <chernetskyy.sergiy@gmail.com>
2018-09-05 22:19:10 +0300

    Was implemented ApiRoute & method v2PriceController@getAllPrices

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        //  $this->call(UsersTableSeeder::class);
-        $this->call(CityTableSeeder::class);
+        $this->call( CityTableSeeder::class);
         $this->call( DiscountTypeTableSeeder::class);
         $this->call(PlaceTableSeeder::class);
         $this->call(PriceTableSeeder::class);
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix'=>'/v2'], function () {
+    Route::get('/getallprices','v2PriceController@getAllPrices');
     Route::get('/getplaces','v2ReservationController@getPlaces');
     Route::get('/getspaces','v2ReservationController@getSpaces');
     Route::get('/chooseplace','v2ReservationController@choosePlace');
