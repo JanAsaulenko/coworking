da679ccad7335458a39c29e54a206d3738f18d31
unknown <mamayserg1@gmail.com>
2018-09-05 19:20:11 +0300

    add components contacts and add servis

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/main/welcome.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAI4IFg_krZ3BflXLNIHCNKocu23lXGV7E'
+    }),
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      { path: 'contacts', component: ContactsComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
     ]),
