def3fd066e6c04db465223e2b5161070bd8b21ff
Che <chernetskyy.sergiy@gmail.com>
2018-09-26 05:31:09 +0300

    shit pulling

--- v2/src/app/app.component.ts
+++ v2/src/app/app.component.ts
@@ -4,8 +4,7 @@ import {PricesService} from "./services/prices.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ PricesService ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'CoWorking';
