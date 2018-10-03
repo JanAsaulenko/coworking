d76d82dd1a216c67d7637e109a38581324abad46
unknown <bormanrenovation6191@gmail.com>
2018-10-03 03:17:29 +0300

    Was implemented 3 select(city, place, space).

--- v2/src/app/components/ordering/ordering.component.ts
+++ v2/src/app/components/ordering/ordering.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-import { NgModel} from '@angular/forms';
-
+import { City } from '../../interfaces/city';
 import {OrderingService} from "../../services/ordering.service";
 
+export class CitySelect {
+    cities: Array<City>;
+    curentCityId: number
+}
 
 
 
@@ -11,29 +14,66 @@ import {OrderingService} from "../../services/ordering.service";
   templateUrl: './ordering.component.html',
   styleUrls: ['./ordering.component.css']
 })
+
+
 export class OrderingComponent implements OnInit {
     cities: any =[];
-    public curentCity: NgModel ;
+    places: any =[];
+    spaces: any =[];
 
+    public curentCityId: any = 0;
+    public curentPlaceId: any = 0;
+    public curentSpaceId: any = 0;
 
   constructor( private orderingService: OrderingService) { }
 
   ngOnInit() {
       this.orderingService.getAllCity().subscribe(
-          (res: any) => {
-              this.cities = res.map((city)=>{
-                  return {
-                      ...city
-                  }
-              });
-              console.log(this.cities);
+          (cities: any) => {
+              this.cities = cities;
           },
           (err) => {
           }
       );
   }
-    onSelect(id:any){
-      console.log(id);
-      this.cities = null;
+
+  onSelectCity(id:any){
+    this.places = [];
+    this.curentPlaceId = 0;
+    this.spaces = [];
+    this.curentSpaceId = 0;
+
+
+        this.orderingService.getPlaces(id).subscribe(
+          (res: any) =>{
+                // console.log(res);
+                this.places = res.map((item:any )=>{
+                    return{
+                        id: item.palce_id,
+                        address: item.address,
+                        name: item.name
+                    }
+                });
+                // console.log(this.places);
+                },
+            (err) =>{
+          });
     }
+
+    onSelectPlace(id:any){
+        this.spaces = [];
+        this.curentSpaceId = 0;
+        this.orderingService.getSpaces(id).subscribe(
+            (res: any) => {
+                this.spaces = res.map((item:any)=>{
+                    return{
+                        id: item.id,
+                        name: item.name
+                    }
+                })
+            },
+            (err) =>{
+            });
+    }
+
 }
--- v2/src/app/services/ordering.service.ts
+++ v2/src/app/services/ordering.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { map } from 'rxjs/operators';
+import {City} from "../interfaces/city";
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +15,25 @@ export class OrderingService {
         private http: HttpClient
     ) { }
 
+
     getAllCity() {
-        return this.http.get(this.getAllCitiesUrl);
+        return this.http.get(this.getAllCitiesUrl)
+            .pipe(map((city: any): City =>{
+
+                return city.map(res => {
+                    return {
+                        id: res.id,
+                        name: res.name
+                    }
+                })
+        }));
     }
 
     getPlaces(id:number){
         if (id != 0 ){
         console.log('getPlaces got not null id params')
         }
-        return this.http.get(this.getPlacesUrl,{ params:{ city_id: id.toString() }})
+        return this.http.get(this.getPlacesUrl,{ params:{ city_id: id.toString() }});
     }
 
     getSpaces(id:number){
