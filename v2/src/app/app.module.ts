ec7ba3739973ef4caa69b8c30d7bb094fdefc2b5
bedTooTheBone <bormanrenovation6191@gmail.com>
2018-10-16 02:40:25 +0300

    fix conflicts

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { AgmCoreModule } from '@agm/core';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {AgmCoreModule} from '@agm/core';
+import {AppComponent} from './app.component';
 import {PricesComponent} from './components/prices/prices.component';
-import { WelcomeComponent } from './components/main/welcome.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { PlacesComponent } from './components/places/places.component';
-import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+import {WelcomeComponent} from './components/main/welcome.component';
+import {ContactsComponent} from './components/contacts/contacts.component';
+import {PlacesComponent} from './components/places/places.component';
+import {AgmSnazzyInfoWindowModule} from '@agm/snazzy-info-window';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from "./material.module";
 import {MatInputModule} from '@angular/material';
-import { OrderingComponent } from './components/ordering/ordering.component';
+import {OrderingComponent} from './components/ordering/ordering.component';
 import {FormsModule} from "@angular/forms";
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import {MAT_DATE_LOCALE} from "@angular/material";
 
@@ -30,30 +27,28 @@ import {MAT_DATE_LOCALE} from "@angular/material";
     OrderingComponent
   ],
   imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      HttpClientModule,
-      MaterialModule,
-      MatInputModule,
-    BrowserModule,FormsModule,
-    HttpClientModule,
+    BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MaterialModule,
+    MatInputModule,
+    FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAI4IFg_krZ3BflXLNIHCNKocu23lXGV7E'
     }),
-      AgmSnazzyInfoWindowModule,
+    AgmSnazzyInfoWindowModule,
     RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: 'prices', component: PricesComponent },
-      { path: 'contacts', component: ContactsComponent },
-      { path: 'places', component: PlacesComponent },
-      { path: 'ordering', component: OrderingComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'prices', component: PricesComponent},
+      {path: 'contacts', component: ContactsComponent},
+      {path: 'places', component: PlacesComponent},
+      {path: 'ordering', component: OrderingComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
     ]),
-    ],
+  ],
   providers: [{provide: MAT_DATE_LOCALE, useValue: 'uk'},],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- v2/src/app/material.module.ts
+++ v2/src/app/material.module.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import {NgModule} from '@angular/core';
 
 import {
@@ -51,28 +51,3 @@ import {
 })
 export class MaterialModule {
 }
-=======
-
-import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material';
-import {MatCardModule} from '@angular/material/card';
-
-
-@NgModule({
-    imports: [MatButtonModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCheckboxModule,
-        MatCardModule,
-        ],
-    exports: [MatButtonModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCheckboxModule,
-        MatCardModule,
-      ],
-})
-export class MaterialModule { }
->>>>>>> remotes/origin/dev
